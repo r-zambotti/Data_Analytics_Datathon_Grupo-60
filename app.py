a7e5cd5525b701ac10e18ed13569d59094a60d8f
r-zambotti <rzambottia@gmail.com>
2025-01-22 00:56:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import seaborn as sns
+import plotly.graph_objects as go
+import plotly.express as px
+import pmdarima as pm
+import warnings
+warnings.filterwarnings(action = 'ignore')
 
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from plotly.subplots import make_subplots
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import acf, pacf, adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from sklearn.metrics import mean_absolute_error, mean_squared_error,accuracy_score
+from sklearn.preprocessing import MinMaxScaler
