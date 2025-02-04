280642b9a69037df875dd7e814251850735c5d4e
Victor Novais <victor.ndo@outlook.com>
2025-02-04 00:38:11 -0300

    Gerando arquivo requirements.txt

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import seaborn as sns
 import plotly.graph_objects as go
