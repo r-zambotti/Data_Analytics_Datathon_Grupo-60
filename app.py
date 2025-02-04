52c9a92705368016eed389d288925e049aa18ac2
Victor Novais <victor.ndo@outlook.com>
2025-02-04 00:42:48 -0300

    deploy da aplicacao no streamlit

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
 # Importar bibliotecas
 import streamlit as st
 import pandas as pd
-import numpy as np
+# import numpy as np
 # import matplotlib.pyplot as plt
-import statsmodels.api as sm
-import seaborn as sns
-import plotly.graph_objects as go
-import plotly.express as px
-import pmdarima as pm
+# import statsmodels.api as sm
+# import seaborn as sns
+# import plotly.graph_objects as go
+# import plotly.express as px
+# import pmdarima as pm
 import requests
 
 #from keras.models import Sequential
 #from keras.layers import LSTM, Dense
-from plotly.subplots import make_subplots
-from prophet import Prophet
-from prophet.plot import plot_plotly
-from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.tsa.stattools import acf, pacf, adfuller
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-from sklearn.metrics import mean_absolute_error, mean_squared_error,accuracy_score
-from sklearn.preprocessing import MinMaxScaler
+# from plotly.subplots import make_subplots
+# from prophet import Prophet
+# from prophet.plot import plot_plotly
+# from statsmodels.tsa.seasonal import seasonal_decompose
+# from statsmodels.tsa.stattools import acf, pacf, adfuller
+# from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+# from sklearn.metrics import mean_absolute_error, mean_squared_error,accuracy_score
+# from sklearn.preprocessing import MinMaxScaler
 
-from sklearn.model_selection import TimeSeriesSplit
-from statsmodels.tsa.stattools import adfuller
+# from sklearn.model_selection import TimeSeriesSplit
+# from statsmodels.tsa.stattools import adfuller
 
 from PIL import Image
 
-import pickle
+# import pickle
 
 from utils import (normality_test, 
                    create_warning, 
